74598354f34f0c5f25751254fc1c17ab5ac7684d
Malachi Spencer <malachispencer@MA309.fritz.box>
2021-01-21 11:17:22 +0000

    BAdded button which loads bottom modal for camera and gallery

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:cupertino_icons/cupertino_icons.dart';
+//import 'package:cupertino_icons/cupertino_icons.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'dart:async';
@@ -17,91 +17,86 @@ class LaetusApp extends StatefulWidget {
 
 class _LaetusAppState extends State<LaetusApp> {
   File _image;
+  final picker = ImagePicker();
 
-  _imgFromCamera() async {
-    File image = await ImagePicker.pickImage(
-        source: ImageSource.camera, imageQuality: 50);
+  Future _getImage(camOrGal) async {
+    ImageSource source;
 
-    setState(() {
-      _image = image;
-    });
-  }
+    camOrGal == 'camera'
+        ? source = ImageSource.camera
+        : source = ImageSource.gallery;
 
-  _imgFromGallery() async {
-    File image = await ImagePicker.pickImage(
-        source: ImageSource.gallery, imageQuality: 50);
+    final pickedFile = await picker.getImage(source: source);
 
     setState(() {
-      _image = image;
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    void _showPicker(context) {
-      showModalBottomSheet(
-        context: context,
-        builder: (BuildContext bc) {
-          return SafeArea(
-            child: Container(
-              child: new Wrap(
-                children: <Widget>[
-                  new ListTile(
-                      leading: new Icon(Icons.photo_library),
-                      title: new Text('Photo Library'),
-                      onTap: () {
-                        _imgFromGallery();
-                        Navigator.of(context).pop();
-                      }),
-                  new ListTile(
-                    leading: new Icon(Icons.photo_camera),
-                    title: new Text('Camera'),
-                    onTap: () {},
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      );
-    }
-
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Laetus"),
-          backgroundColor: Colors.blueGrey,
-        ),
-        body: Image.asset('assets/images/sample_image.jpg'),
-        bottomNavigationBar: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              FloatingActionButton(
-                onPressed: () {
-                  _imgFromCamera();
-                  Navigator.of(context).pop();
-                },
-                backgroundColor: Colors.blueGrey,
-                child: Theme.of(context).platform == TargetPlatform.iOS
-                    ? Icon(CupertinoIcons.camera)
-                    : Icon(Icons.camera),
-              ),
-              FloatingActionButton(
-                onPressed: () {
-                  _imgFromCamera();
-                  Navigator.of(context).pop();
-                },
-                backgroundColor: Colors.blueGrey,
-                child: Theme.of(context).platform == TargetPlatform.iOS
-                    ? Icon(CupertinoIcons.camera)
-                    : Icon(Icons.camera),
-              ),
-            ],
-          ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Laetus"),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Center(
+        child: _image == null
+            ? Image.asset('assets/images/sample_image.jpg')
+            : Image.file(_image),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Builder(
+        builder: (context) => FloatingActionButton(
+          child: Theme.of(context).platform == TargetPlatform.iOS
+              ? Icon(CupertinoIcons.camera)
+              : Icon(Icons.add_a_photo),
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return Container(
+                    color: Color(0xFF737373),
+                    child: Container(
+                      height: 120,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).canvasColor,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                        ),
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          ListTile(
+                            leading: Icon(Icons.photo_library),
+                            title: Text('Gallery'),
+                            onTap: () {
+                              _getImage('gallery');
+                              Navigator.pop(context);
+                            },
+                          ),
+                          ListTile(
+                              leading: Icon(Icons.photo_camera),
+                              title: Text('Camera'),
+                              onTap: () {
+                                _getImage('camera');
+                                Navigator.pop(context);
+                              }),
+                        ],
+                      ),
+                    ),
+                  );
+                });
+          },
         ),
       ),
-    );
+    ));
   }
 }
 
