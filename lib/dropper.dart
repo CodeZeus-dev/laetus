6eeacaeb6ef1260201dc051938b99dd7c8f66f3d
Chris Walsh <highwalshy@gmail.com>
2021-01-26 12:12:23 +0000

    Started on logic to flip icon when too close to borders

--- lib/dropper.dart
+++ lib/dropper.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Dropper extends StatelessWidget {
@@ -7,34 +9,37 @@ class Dropper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 60,
-      height: 85,
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: Container(
-              child: Image.asset(
-                'assets/images/dropper.jpeg',
-                scale: 7.0,
+    return Transform(
+      transform: Matrix4.identity()..rotateX(180 / 180 * pi),
+      child: Container(
+        width: 60,
+        height: 85,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: Container(
+                child: Image.asset(
+                  'assets/images/dropper.jpeg',
+                  scale: 7.0,
+                ),
               ),
             ),
-          ),
-          Positioned(
-            top: 0,
-            right: 0,
-            child: Container(
-              width: 40.0,
-              height: 40.0,
-              decoration: BoxDecoration(
-                color: colour,
-                borderRadius: BorderRadius.circular(12),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: Container(
+                width: 40.0,
+                height: 40.0,
+                decoration: BoxDecoration(
+                  color: colour,
+                  borderRadius: BorderRadius.circular(12),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
